60e022dd6580f097f44ff406a7d4777abb36570d
Aravind pj <aravindjayan1245@gmail.com>
2023-07-27 18:07:11 +0530

    booking detail page added

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient=useQueryClient()
   const [searchParam] = useSearchParams();
   const filterValue = searchParam.get("status");
   const filter =
@@ -20,5 +22,21 @@ export function useBookings() {
     queryKey: ["bookings", filter,sortBy,page],
     queryFn: () => getBookings({filter,sortBy,page}),
   });
+
+  //PRE-FETCH
+  // in this example fetching next page data already
+  const pageCount=Math.ceil(page/PAGE_SIZE)
+  if(page>pageCount) // THIS CONDITION TO AVOID UNWANTED FETCHING DATA EX: THERE IS 3 PAGES IF REACHING 3 RD PAGE THIS PREFECH METHOD TRY TO FETCH NEXT DATA FOR 4TH PAGE THAT DATA IS EMPTY ARRAY [] (BCZ 30 results divde 10 = 3 page ) WE DONT NEED THAT
+  // the prefectch is usefull to fetch next data avoid loadig screen 
+  queryClient.prefetchQuery({
+      queryKey: ["bookings", filter,sortBy,page+1],
+      queryFn: () => getBookings({filter,sortBy,page:page+1}),
+    })
+
+  if(page<pageCount)  
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter,sortBy,page-1],
+      queryFn: () => getBookings({filter,sortBy,page:page-1}),
+    })
   return { isLoading, bookings , count};
 }
